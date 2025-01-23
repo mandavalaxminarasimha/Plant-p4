aaadb78dbee63cf6c59a6926fa4a59e77debdd9d
mandavalaxminarasimha <22r91a7431@tkrec.ac.in>
2025-01-23 16:27:42 +0530

    Add files via upload

---
+++ t.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+training_set = tf.keras.utils.image_dataset_from_directory(
+    'C:/Users/rahul/OneDrive/文档/ln dataset/Dataset/train',
+    labels="inferred",
+    label_mode="categorical",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(128, 128),
+    shuffle=True,
+    seed=None,
+    validation_split=None,
+    subset=None,
+    interpolation="bilinear",
+    follow_links=False,
+    crop_to_aspect_ratio=False
+)
